b03989872887564c9f10cc16a7192cd619790d09
alicemoss <alice@moss.nu>
2026-02-08 20:07:05 +0100

    test av olika saker

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const blocks = document.querySelectorAll("[data-include]");
+  
+    for (const el of blocks) {
+      const file = el.getAttribute("data-include");
+      const res = await fetch(file, { cache: "no-store" });
+      el.innerHTML = await res.text();
+    }
+  });
+  
